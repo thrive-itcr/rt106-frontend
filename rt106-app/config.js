7b26cceb0cd43091751bf03959cdabaa3da247b5
Sarachan, Brion D <sarachan@ge.com>
2018-03-09 12:33:34 -0500

    Fixed webrebuild.sh.

--- rt106-app/config.js
+++
@@ -1,7 +0,0 @@
-angular.module("rt106.config", [])
-
-    .constant("API_VERSION", "v1")
-
-    .constant("Rt106_SERVER_URL", "http://localhost")
-
-;
--- webrebuild.sh
+++ webrebuild.sh
@@ -7,12 +7,11 @@ docker rm rt106_web_1
 
 echo ""
 echo "Removing web image."
-docker rmi rt106/rt106-frontend
+docker rmi thriveitcr/rt106-frontend
 
 echo ""
 echo "Building web image."
-docker build -t rt106/rt106-frontend -f Dockerfile .
-docker build -t rt106/rt106-frontend  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy  .
+docker build -t thriveitcr/rt106-frontend  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy  .
 
 echo ""
 docker images
